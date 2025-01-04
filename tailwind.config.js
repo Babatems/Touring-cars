511f18d9c79ea02afe4cc9d76d35ccaffa030f5e
Inumoh Temi <temiloluwainumoh@gmail.com>
2025-01-04 22:31:12 +0100

    Added new pages, dependencies and updated some files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
